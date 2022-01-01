caa55ace31ced20a2e3f6eb83d7163fc01d52c4c
busyboyshubro <shubropurkait6@gmail.com>
2022-01-01 23:47:46 +0530

    href

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Banner() {
     return (
@@ -13,7 +14,7 @@ function Banner() {
                   <p>Pixie HTML Template can be converted into your desired CMS theme. Total <strong>5 pages</strong> included. You can use this Bootstrap v4.1.3 layout for any CMS. 
                   <br /><br />Please tell your friends about <a rel="nofollow" href="https://www.facebook.com//">SP</a> free template site. Thank you. Photo credit goes to <a rel="nofollow" href="https://www.pexels.com">Pexels website</a>.</p>
                   <div className="main-button">
-                    <a>Order Now!</a>
+                    <Link to="/Products">Order Now!</Link>
                   </div>
                 </div>
               </div>
